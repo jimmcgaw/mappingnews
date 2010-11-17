e373873359bfc0ecc02ad0b6af8b95e664f51a40
Jim McGaw <jim@greensherpa.com>
2010-11-16 16:43:54 -0800

    Mapping news

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,21 +1,36 @@
 require 'uri'
 require 'rss'
 
+# dynamically add lat and long properties to TSS feed items
+class RSS::Rss::Channel::Item
+  attr_accessor :latitude, :longitude  
+end
+
 class NewsController < ApplicationController
   layout 'application'
+  
+  
   
   before_filter :intialize_session_search
   
   def search
     @recent_searches = @search.search_terms.find(:all, 
-                                               :limit => 10,
-                                               :order => "updated_at DESC" )
+                                                 :limit => 10,
+                                                 :order => "updated_at DESC" )
     
-    if params[:q]
+    if request.post? and params[:q]
       @q = params[:q]
-      store_search(@q)
       @rss = get_news(@q)
     end
+    
+    respond_to do |format|
+      if request.xhr?
+        format.js { render :partial => 'news_result', :collection => @rss, :as => :item }
+      else
+        format.html { render :layout => true }
+      end
+    end
+    
   end
   
   private
@@ -24,17 +39,28 @@ class NewsController < ApplicationController
     search_text = search_text.downcase
     search_text = URI.escape(search_text)
     url = "http://news.google.com/news?pz=1&cf=all&ned=us&hl=en&q=#{search_text}&cf=all&output=rss"
-    puts url
-    RSS::Parser.parse(open(url).read, false).items[0..10]
-  end
-  
-  def store_search(search_text)
-    past_search = @search.search_terms.find_by_keywords(search_text)
-    if past_search
-      past_search.updated_at = DateTime.now
-      past_search.save  # update timestamp
-    else
-      SearchTerm.create( :keywords => search_text, :search => @search)
+    rss = RSS::Parser.parse(open(url).read, false).items[0..10]
+    
+    tags = Tag.find(:all)
+    
+    if rss
+      rss.each do |item|
+        item.latitude = nil
+        item.longitude = nil
+        tags.each do |tag|
+          if item.title.index(tag.name) || item.description.index(tag.name)
+            mp = tag.mappoints[0]
+            if mp
+             item.latitude = mp.lat
+              item.longitude = mp.long
+            end
+          end
+        end
+        
+      end
     end
+    
+    
+    rss
   end
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -9,11 +9,9 @@ MappingNews.geo_success = function(p){
 	
 	var po = org.polymaps;
 	
-	alert(jQuery("#map").has("svg"));
-
 	var map = po.map()
 		.center({lat: latitude, lon: longitude})
-		.zoom(9.00)
+		.zoom(1.00)
 	    .container(document.getElementById("map").appendChild(po.svg("svg")))
 	    .add(po.interact());
 	    //.add(po.hash());
@@ -26,17 +24,94 @@ MappingNews.geo_success = function(p){
 	    .hosts(["a.", "b.", "c.", ""])));
 	
 	//map.add(po.compass().pan("none"));
-	alert(jQuery("#map").has("svg"));
 };
 
 MappingNews.geo_error = function(){ 
-	alert("Could not find you!"); 
+	//alert("Could not find you!"); 
 };
 
-MappingNews.prepareDocument = function(){
+MappingNews.load_results = function(e){
+	e.preventDefault();
+	var search_form = jQuery("form#search_form");
+	var search_box = jQuery("form#search_form input#q");
+	if(search_box.val().length < 3){
+		jQuery('#results').html("");
+		return;
+	}
+	jQuery.ajax({
+		url: search_form.attr('action'),
+		data: search_form.serialize(),
+		dataType: "html",
+		type: search_form.attr('method'),
+		success: function(html){
+			jQuery('#results').html(html);
+			MappingNews.activate_links();
+		},
+		error: function(xhr, text, error){
+			//alert(error);
+		}
+	});
+};
+
+MappingNews.activate_links = function(){
+	var news_links = jQuery("div#results a");
+	news_links.each(function(index){
+		jQuery(news_links[index]).click(function(){
+			var search_terms = jQuery("form#search_form input#q").val();
+			jQuery.ajax({
+				url: jQuery("#store_search_url").val(),
+				type: "POST",
+				data: {'q': search_terms },
+				success: function(){},
+				error: function(){}
+			});
+		});
+	});
+};
+
+MappingNews.prepareSearchForm = function(){
+	var search_form = jQuery("form#search_form");
+	search_form.submit(MappingNews.load_results);
+	var search_box = jQuery("form#search_form input#q");
+	search_box.keyup(jQuery.defer(600, MappingNews.load_results));
+};
+
+MappingNews.prepareRecentSearchLinks = function(){
+	var search_box = jQuery("form#search_form input#q");
+	var past_search_links = jQuery("#past_searches a");
+	past_search_links.each(function(index){
+		jQuery(past_search_links[index]).click(function(e){
+			search_box.val(jQuery(this).text());
+			MappingNews.load_results(e);
+		});
+	});
+};
+
+MappingNews.load_my_map = function(){
 	if (geo_position_js.init()){ 
 		geo_position_js.getCurrentPosition(MappingNews.geo_success, MappingNews.geo_error); 
 	}
+};
+
+var ol_map, layer;
+
+MappingNews.load_news_map = function(){
+	ol_map = new OpenLayers.Map('map', {maxResolution:'auto'});
+	layer = new OpenLayers.Layer.WMS( "OpenLayers WMS",
+                "http://vmap0.tiles.osgeo.org/wms/vmap0", {layers: 'basic'} );
+    ol_map.addLayer(layer);
+    ol_map.setCenter(new OpenLayers.LonLat(0, 0), 0);
+    //ol_map.addControl(new OpenLayers.Control.LayerSwitcher());
+    //var newl = new OpenLayers.Layer.GeoRSS( 'GeoRSS', 'georss.xml');
+    //ol_map.addLayer(newl);
+};
+
+MappingNews.prepareDocument = function(){
+	MappingNews.prepareSearchForm();
+	MappingNews.prepareRecentSearchLinks();
+	
+	//MappingNews.load_my_map();
+	MappingNews.load_news_map();
 };
 
 jQuery(document).ready(MappingNews.prepareDocument);
